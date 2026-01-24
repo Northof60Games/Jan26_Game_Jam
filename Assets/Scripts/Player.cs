adc87117bbf8377010ddfdd30ebd4e26ef432c32
Northof60 <213980084+Northof60Games@users.noreply.github.com>
2026-01-23 18:24:20 -0800

    Setup initial variables

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,27 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
+    // Initial variables
+    [SerializeField] private float thrust= 5f;
+    [SerializeField] private float turnSpeed = 180f;
+    [SerializeField] private float maxSpeed = 15f;
+
+    private float linearDragWhileThrusting = 0.3f;
+    private float linearDragNotThrusting = 0f;
+    
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float turnInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
